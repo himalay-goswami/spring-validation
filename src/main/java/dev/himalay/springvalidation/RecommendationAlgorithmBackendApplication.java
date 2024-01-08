091948e002db8621611aeb07daa9016052972bb7
himalaygoswami <himalayagswm@gmail.com>
2024-01-08 19:55:07 +0530

    third commit.

---
+++ src/main/java/dev/himalay/springvalidation/RecommendationAlgorithmBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.himalay.springvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationAlgorithmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationAlgorithmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/himalay/springvalidation/RecommendationAlgorithmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.himalay.springvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationAlgorithmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
